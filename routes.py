2be346aa7b882348c0ef97bcc7280384b5f89959
Tim Fleming <tfleming@flemm-aspire.flem-geo.com>
2017-03-20 10:44:38 -0600

    Complete user model configuration

--- routes.py
+++ routes.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from models import db
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/flaskify'
+db.init_app(app)
 
 @app.route("/")
 def index():
@@ -9,7 +13,6 @@ def index():
 @app.route("/about")
 def about():
 	return render_template("about.html")
-
 
 if __name__ == "__main__":
   app.run(debug=True)
