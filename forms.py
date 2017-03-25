2608addd79f1715c05af4573b5a8d156fbe09146
Tim Fleming <tfleming@flemm-aspire.flem-geo.com>
2017-03-25 12:47:00 -0600

    Complete login and logout functionality

--- forms.py
+++ forms.py
@@ -9,3 +9,7 @@ class SignupForm(Form):
   password = PasswordField('Password', validators = [DataRequired("Enter your password."), Length(min=6, message="Must be 6 characters or more.")])
   submit = SubmitField('sign up')
 
+class LoginForm(Form):
+  email = StringField('Email', validators = [DataRequired("Enter your email address."), Email("Enter your email address")])
+  password = PasswordField('Password', validators = [DataRequired("Enter a password.")])
+  submit = SubmitField("Sign in")
