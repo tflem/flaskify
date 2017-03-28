ba51c00541539aa30aa684af8d52b39d6f147245
Tim Fleming <tfleming@flemm-aspire.flem-geo.com>
2017-03-28 10:07:47 -0600

    Heroku db configuration

--- run.py
+++ run.py
@@ -1,9 +1,22 @@
 import os
+import psycopg2
+import urlparse
 
 from app import create_app
 
 config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
+
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
 if __name__ == '__main__':
     app.run()
