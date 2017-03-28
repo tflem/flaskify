ce5dec55be2c534f80cd636cf3d74c148a6baa82
Tim Fleming <tfleming@flemm-aspire.flem-geo.com>
2017-03-28 09:18:48 -0600

    Application restructure

---
+++ app/auth/views.py
@@ -0,0 +1,50 @@
+from flask import flash, redirect, render_template, url_for
+from flask_login import login_required, login_user, logout_user
+
+from . import auth
+from forms import LoginForm, RegistrationForm
+from .. import db
+from ..models import Employee
+
+# Register route, adds employee to database via registration from
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		employee = Employee(email=form.email.data,
+							username=form.username.data,
+							first_name=form.first_name.data,
+							last_name=form.last_name.data,
+							password=form.password.data)
+		db.session.add(employee)
+		db.session.commit()
+		flash('You have successfully registered! You can now login.')
+
+		# Redirect to login page
+		return redirect(url_for('auth.login'))
+	# Load registration template
+	return render_template('auth/register.html', form=form, title='Register')
+
+# Login route, logs employee in via login form
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		
+		# check whether employee exists in the database and whether
+        # the password entered matches the password in the database
+		employee = Employee.query.filter_by(email=form.email.data).first()
+		if employee is not None and employee.verify_password(form.password.data):
+			login_user(employee)
+			return redirect(url_for('home.dashboard'))
+		else:
+			flash('Invalid email or password.')
+
+	return render_template('auth/login.html', form=form, title='Login')
+
+@auth.route('/logout')
+def logout():
+	logout_user()
+	flash('You have successfully been logged out.')
+	return redirect(url_for('auth.login'))
+
---
+++ app/home/views.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from flask_login import login_required
+
+
+from . import home
+
+
+@home.route('/')
+def homepage():
+	return render_template('home/index.html', title="Welcome")
+
+@home.route('/about')
+def about():
+	return render_template('home/about.html', title="About")
+
+@home.route('/dashboard')
+@login_required
+def dashboard():
+	return render_template('home/dashboard.html', title="Dashboard")
