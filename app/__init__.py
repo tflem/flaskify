0c5bb1bbc7e353ba4aee0d579b44dd93be5a1a81
Tim Fleming <tfleming@flemm-aspire.flem-geo.com>
2017-03-31 11:35:24 -0600

    Fix indentation error in init file

--- app/__init__.py
+++ app/__init__.py
@@ -13,9 +13,10 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app(config_name):
-	app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+    db.init_app(app)
 
     
     Bootstrap(app)
