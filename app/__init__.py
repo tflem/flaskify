ee4a3aed0b5bd614104775a938b9b03c1e26f1f9
Tim Fleming <tfleming@flemm-aspire.flem-geo.com>
2017-04-01 05:31:36 -0600

    Heroku configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-# third-party imports
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
@@ -8,17 +7,14 @@ from flask_sqlalchemy import SQLAlchemy
 # local imports
 from config import app_config
 
-# db variable initialization
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
-	app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')    
-    db.init_app(app)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
 
-    
     Bootstrap(app)
     db.init_app(app)
     login_manager.init_app(app)
@@ -36,6 +32,5 @@ def create_app(config_name):
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
-
 
     return app
