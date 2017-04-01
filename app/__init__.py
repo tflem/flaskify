fd64f15fb5dde05cc88ce68f57337b51e05b8f43
Tim Fleming <tfleming@flemm-aspire.flem-geo.com>
2017-04-01 06:20:24 -0600

    Heroku db config

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ login_manager = LoginManager()
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    app.config[SQLALCHEMY_DATABASE_URI] = os.environ['DATABASE_URL']
     app.config.from_pyfile('config.py')
 
     Bootstrap(app)
