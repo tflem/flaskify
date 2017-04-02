7402d8350d288cb467651fef392505193f78bcad
Tim Fleming <tfleming@flemm-aspire.flem-geo.com>
2017-04-01 18:00:48 -0600

    Update config file

--- instance/config.py
+++ instance/config.py
@@ -1,6 +1,5 @@
 import os
 
 SECRET_KEY = 'w2\xf3!V\xfc\xc0r\xc5~U1\xcb\x17\x84Z\xa8\xed\xb6\xb1\xe9\x89\xa5.'
-#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Fa37PhAn@localhost/flaskify_db'
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
             'postgresql+psycopg2://postgres:Fa37PhAn@localhost/flaskify_db')
