556a255ad911a6a87d2709e065bcaaff210d28ca
Tim Fleming <tfleming@flemm-aspire.flem-geo.com>
2017-05-13 14:54:17 -0600

    Update init file for production

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 # local imports
 from config import app_config
@@ -11,9 +12,16 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+    if os.getenv('FLASK_CONFIG') == "production":
+        app = Flask(__name__)
+        app.config.update(
+            SECRET_KEY=os.getenv('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+        )
+    else:
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(app_config[config_name])
+        app.config.from_pyfile('config.py')
 
     Bootstrap(app)
     db.init_app(app)
