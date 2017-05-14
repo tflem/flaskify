0e6e2a019ce9b62b393d6ae1486f7e2ccd58da75
Tim Fleming <tfleming@flemm-aspire.flem-geo.com>
2017-05-13 18:10:04 -0600

    Minor UI and configuration changes

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-import os
 
 # local imports
 from config import app_config
